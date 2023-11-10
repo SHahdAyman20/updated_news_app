853e9bc5e0276ce7a2df379307a34892122c9ca5
SHahdAyman20 <shahdmanal221239@gmail.com>
2023-11-11 00:51:08 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/instant_api_news_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.instant_api_news_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/news_details.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:instant_api_news_app/json_data/article_model.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class NewsDetailsScreen extends StatefulWidget {
+
+  final String url;
+
+   const NewsDetailsScreen({Key? key, required this.url}) : super(key: key);
+
+  @override
+  State<NewsDetailsScreen> createState() => _NewsDetailsScreenState();
+}
+
+class _NewsDetailsScreenState extends State<NewsDetailsScreen> {
+  final controller = WebViewController();
+
+  @override
+  void initState() {
+    super.initState();
+    webView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.indigo[800],
+        title: const Text(
+          'News Details',
+          style: TextStyle(
+            fontSize: 35,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+      ),
+      body: WebViewWidget(
+        controller:controller,
+      ),
+    );
+  }
+
+  void webView(){
+     controller
+       .loadRequest(
+         Uri.parse(widget.url));
+  }
+}
